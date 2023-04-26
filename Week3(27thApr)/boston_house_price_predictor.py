98f76109263d7f45a202e71b87314abae1f17f7a
DrSavage <106464447+BharathwajManoharan@users.noreply.github.com>
2023-04-27 00:42:24 +0530

    added

--- Week3(27thApr)/boston_house_price_predictor.py
+++ Week3(27thApr)/boston_house_price_predictor.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 import pickle
 
 # Load Boston dataset from Scikit-Learn library
@@ -16,9 +18,28 @@ y = boston['PRICE']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Fit a linear regression model to the training data
-model = LinearRegression()
-model.fit(X_train, y_train)
+linear_model = LinearRegression()
+linear_model.fit(X_train, y_train)
 
-# Save the trained model to a file using Pickle
+# Fit a decision tree regression model to the training data
+dt_model = DecisionTreeRegressor(random_state=42)
+dt_model.fit(X_train, y_train)
+
+# Fit a random forest regression model to the training data
+rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+
+# Fit a gradient boosting regression model to the training data
+gb_model = GradientBoostingRegressor(n_estimators=100, random_state=42)
+gb_model.fit(X_train, y_train)
+
+# Evaluate the performance of each model on the test data
+print("Linear Regression R-squared:", linear_model.score(X_test, y_test))
+print("Decision Tree Regression R-squared:", dt_model.score(X_test, y_test))
+print("Random Forest Regression R-squared:", rf_model.score(X_test, y_test))
+print("Gradient Boosting Regression R-squared:", gb_model.score(X_test, y_test))
+
+# Save the best performing model to a file using Pickle
+best_model = max(linear_model, dt_model, rf_model, gb_model, key=lambda model: model.score(X_test, y_test))
 with open('model.pkl', 'wb') as f:
-    pickle.dump(model, f)
+    pickle.dump(best_model, f)
